7bbdfdee59a9cf63ba9a3999687b7116757a677c
Yoannis Jamar <yoannis.j@gmail.com>
2015-11-30 13:17:32 +0100

    rounded buttons + input-group (WIP)
    Signed-off-by: Yoannis Jamar <yoannis.j@gmail.com>

--- src/assets/scripts/widgets/multifold.js
+++ src/assets/scripts/widgets/multifold.js
@@ -42,7 +42,7 @@ var MultiFold = module.exports = Fold.extend({
     // get bottom of lowest item in 'minItems'
     this.$items.slice(0, this._minItems()).each(function() {
       var $itm = $(this),
-        itmBottom = $itm.offset().top + $itm.outerHeight(true);
+        itmBottom = $itm.offset().top + $itm.outerHeight();
 
       lowest = Math.max(lowest, itmBottom);
     });
