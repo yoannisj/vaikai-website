54958ca2021acb0620a3cbe9d502f13f0646a2a0
Matas Petrikas <matas.petrikas@gmail.com>
2015-12-06 21:32:11 +0100

    images added, the units config moved to yaml

--- src/assets/scripts/preorder.js
+++ src/assets/scripts/preorder.js
@@ -1,3 +1,29 @@
+
+function getUrlArguments() {
+  var search = location.search.substring(1);
+  if (search) {
+    return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+  } else {
+    return {};
+  }
+  
+}
+
+// pre-fill coupon information if there's one
+var search = getUrlArguments();
+if (search.coupon) {
+  $('[data-celery]').attr('data-celery-prefill-coupon', search.coupon);
+}
+
+
 $('select.js-select').on('change', function(event){
-  alert('i am selected');
+  var $select = $(this),
+      selection = $select.val();
+      
+
+      // hide all
+      $('[data-vaikai-option]').hide();
+      // show only the right one
+      $('[data-vaikai-option='+selection+']').show();
+
 });
